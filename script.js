1dabdc85aa9568de4bb9e56528f0427b05407354
Khatereh Abron <khatereh.abron@gmail.com>
2025-02-07 09:53:42 +0100

    Displaying Data

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
-const apiKey = 'f98gJUkGDG6AHJF3WxYJZJ70FwWOVCjyJSyq3Hu9AjU';
+const count = 30;
+const apiKey = 'zDqXOA52flLX3hJxIVgihdZCuCtJ1GcQ9-cSBZjeXQ8';
 
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+//check if all the images were loaded
+function imageLoaded() {
+
+    imagesLoaded++;
+
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+
+    }
+}
+
 
 //Helper function to set attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -17,6 +34,9 @@ function setAttributes(element, attributes) {
 
 //Create elements for links and photos , add to dom
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // Create <a> to link to Unsplash
@@ -40,6 +60,9 @@ function displayPhotos() {
 
         });
 
+        //Event Listener,check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+
         //Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -62,6 +85,15 @@ async function getPhotos() {
         // Catch Error Here
     }
 }
+//Check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+
+    }
+});
+
 
 //On loads
 getPhotos();
